07eb48b053073ef7d39ef6e240a18d410df58681
Lucas Bryan <lucasbryan0217@gmail.com>
2022-11-25 14:53:33 -0300

    feat: create initial structure themes

--- src/styles/customTheme.ts
+++ src/styles/customTheme.ts
@@ -1,12 +1,38 @@
 import { extendTheme } from '@stardust-ds/react';
 
-const theme = {
+type DynamicSchemeColors = {
   color: {
-    primary: {
-      pure: 'red',
-    },
-    ubistart: 'blue',
-  },
+    bg: string;
+    text: string;
+  };
 };
 
-export const customTheme = extendTheme(theme);
+const theme = extendTheme({});
+
+export type Theme = typeof theme & {
+  brand: DynamicSchemeColors;
+};
+
+export enum COLOR_SCHEME {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
+export type Scheme = 'light' | 'dark';
+
+export const modes: Record<Scheme, DynamicSchemeColors> = {
+  light: { color: { bg: '#FFF', text: '#000' } },
+  dark: { color: { bg: '#22272D', text: '#fff' } },
+};
+
+export const preferredMode = window?.matchMedia('(prefers-color-scheme: dark)')
+  ?.matches
+  ? 'dark'
+  : 'light';
+
+export function getTheme(scheme: Scheme) {
+  const deepStringifyClone = JSON.stringify(extendTheme(modes[scheme]));
+  const deepParseClone = JSON.parse(deepStringifyClone);
+
+  return deepParseClone;
+}
