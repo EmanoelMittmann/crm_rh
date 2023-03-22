7075c633a809c80eb60d70da44ecdbbfbe576517
José Barros <61697278+zzzbarros@users.noreply.github.com>
2023-03-22 18:35:10 -0300

    feat: create Input atom with variants
    - Add Icon input
    - Add Password input

---
+++ src/components/atoms/Inputs/Icon/index.tsx
@@ -0,0 +1,6 @@
+import { IInputsProps } from 'react-app-env'
+import { ContainerPositionIcon } from './style'
+
+export default ({ Icon }: IInputsProps) => {
+  return <ContainerPositionIcon>{Icon}</ContainerPositionIcon>
+}
---
+++ src/components/atoms/Inputs/Password/index.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { Input, InputProps } from '@stardust-ds/react'
+import { IconEyeSlash, IconEye } from 'components/atoms'
+
+interface Props extends Omit<InputProps, 'type'> {}
+
+export default (props: Props) => {
+  const [show, setShow] = useState(false)
+
+  const type = show ? 'text' : 'password'
+  const icon = !show ? <IconEyeSlash /> : <IconEye />
+
+  const togglePasswordType = () => setShow(!show)
+
+  return (
+    <Input
+      isFullWidth
+      label='Senha'
+      placeholder='senha'
+      iconRight={icon}
+      iconRightAction={togglePasswordType}
+      {...props}
+      type={type}
+    />
+  )
+}
