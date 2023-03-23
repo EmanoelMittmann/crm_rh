001b68d112b1d36711890347f4a17a8684022423
José Barros <61697278+zzzbarros@users.noreply.github.com>
2023-03-23 18:07:48 -0300

    chore: added option props to button types and added index file into utils

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,14 +2,14 @@ import { type } from "os";
 import { MouseEvent } from "react";
 
 export interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  Text: string;
-  Icon: JSX.Element;
-  color: string;
-  fill: string;
-  fillActive: string;
-  colorActive: string;
-  bgActive: string;
-  isActive: boolean;
+  Text: string
+  Icon: JSX.Element
+  color: string
+  fill: string
+  isActive?: boolean
+  fillActive?: string
+  colorActive?: string
+  bgActive?: string
 }
 
 export interface IInputsProps {
