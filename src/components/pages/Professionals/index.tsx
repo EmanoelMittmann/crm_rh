23f2cd166d718b071f869a803f47e9a436e3a65b
José Barros <61697278+zzzbarros@users.noreply.github.com>
2023-03-24 17:09:13 -0300

    feat: add list template into professionals page

--- src/components/pages/Professionals/index.tsx
+++ src/components/pages/Professionals/index.tsx
@@ -1,11 +1,37 @@
-import ProfessionalsMain from '../../organisms/Professionals'
-import MasterPage from '../MasterPage/index'
+import { AuthTemplate, ListTemplate } from 'components/templates'
+import { Table, Filter } from 'components/organisms'
+
+const MOCK = [
+  {
+    id: '6391f2d5b92ad76f6ab2394e',
+    avatar: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
+    name: 'Lynne Gilmore',
+    email: 'lynnegilmore@accupharm.com',
+    status: true,
+    company: 'UBISTART',
+    phone: '(826) 470-3995',
+    address: '311 Stryker Court, Martinez, Tennessee, 3122',
+  },
+  {
+    id: '6391f2d5b92ad76f6ab2393e',
+    avatar: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
+    name: 'Lynne Gilmore',
+    email: 'lynnegilmore@accupharm.com',
+    status: true,
+    company: 'UBISTART',
+    phone: '(826) 470-3995',
+    address: '311 Stryker Court, Martinez, Tennessee, 3122',
+  },
+]
 
 const Professionals = () => {
   return (
-    <MasterPage>
-        <ProfessionalsMain/>
-    </MasterPage>
+    <AuthTemplate>
+      <ListTemplate title='Profissionais'>
+        <Filter.Professionals />
+        <Table.Professionals data={MOCK} />
+      </ListTemplate>
+    </AuthTemplate>
   )
 }
 
