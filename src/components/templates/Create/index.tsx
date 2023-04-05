fb0db58b25279afbc1cb8a07f903eb2692602064
José Barros <61697278+zzzbarros@users.noreply.github.com>
2023-04-05 11:58:25 -0300

    feat: added Create Page template

---
+++ src/components/templates/Create/index.tsx
@@ -0,0 +1,28 @@
+import type { ReactNode } from 'react'
+import { Typography } from '@stardust-ds/react'
+import { IconLeftArrow } from 'components/atoms'
+import { theme } from 'styles'
+import { Container, Button, Main } from './style'
+
+interface Props {
+  title: string
+  children?: ReactNode
+}
+
+const FONT_COLOR = theme.neutrals.gray8
+
+export default ({ title, children }: Props) => {
+  return (
+    <Main>
+      <Container>
+        <Button title='Voltar'>
+          <IconLeftArrow fill={FONT_COLOR} />
+        </Button>
+        <Typography type='h3' color={FONT_COLOR}>
+          {title}
+        </Typography>
+      </Container>
+      <Container>{children}</Container>
+    </Main>
+  )
+}
