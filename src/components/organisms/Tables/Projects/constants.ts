0c656e9c5974e4994d2118cd66966ccf1bd54e00
Raniere Rocha <raniere.rocha@ubistart.com>
2023-05-14 23:44:35 -0300

    feat: create Project Table

---
+++ src/components/organisms/Tables/Projects/constants.ts
@@ -0,0 +1,44 @@
+export const GRID_TEMPLATE =
+  '1fr 1fr .8fr .8fr .3fr .8fr .8fr .3fr .2fr ' //'repeat(9, 1fr)'
+export const HEADERS = [
+  {
+    field: 'name',
+    label: 'Projeto',
+    on: false
+  },
+  {
+    field: 'date_start',
+    label: 'Início',
+    on: false
+  },
+  {
+    field: 'hours_mounths_estimated',
+    label: 'H/ Mensais Estimadas',
+    on: false
+  },
+  {
+    field: 'hours_mounths_performed',
+    label: 'H/ Mensais Realizadas',
+    on: false
+  },
+  {
+    field: 'hours_mounths_percent',
+    label: '%',
+    on: false
+  },
+  {
+    field: 'extra_hours_estimated',
+    label: 'H/ Extras Estimadas',
+    on: false
+  },
+  {
+    field: 'extra_hours_performed',
+    label: 'H/ Extras Realizadas',
+    on: false
+  },
+  {
+    field: 'extra_hours_percent',
+    label: '%',
+    on: false
+  }
+]
