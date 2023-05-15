590a21a9c302c677c8478472be41dd75b6ca3b51
Raniere Rocha <raniere.rocha@ubistart.com>
2023-05-14 23:43:58 -0300

    feat: add new api and external routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,9 @@ export const routes = {
     list: '/professionals',
     updateStatus: (id: number) => `/user/updateStatus/${id}`,
     validateCPF: '/user/validateCpf',
-    register: '/user'
+    register: '/user',
+    userProjects: (id: number) => `/userProjects/user/${id}`,
+    getUser: (id: number) => `/user/${id}`
   },
   job: {
     list: '/job'
@@ -16,10 +18,15 @@ export const routes = {
   },
   project: {
     list: '/project'
+  },
+  companies: {
+    list: '/companies'
   }
 }
 
 export const externRoutes = {
   banks: 'https://brasilapi.com.br/api/banks/v1',
+  cnpj: (cnpj: string) =>
+    `https://brasilapi.com.br/api/cnpj/v1/${cnpj}`,
   cep: (search: string) => ` https://viacep.com.br/ws/${search}/json/`
 }
