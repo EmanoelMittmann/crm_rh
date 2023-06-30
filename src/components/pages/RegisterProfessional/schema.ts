f1c6ad55cef1e7333cd05e3fa4d3655bedf2e9fb
EmanoelMittmann <emanoelmittmann@hotmail.com>
2023-06-30 11:00:12 -0300

    fix: [Form Professional] page/schema.ts adjustment in validations

--- src/components/pages/RegisterProfessional/schema.ts
+++ src/components/pages/RegisterProfessional/schema.ts
@@ -1,73 +1,108 @@
-import { object, string, number, date, array } from 'yup'
+import { object, string, number, date, array, mixed } from 'yup'
 
 import { handleRemoveSpecialCharacters } from './logic'
+export const validation = {
+  required: 'Campo Obrigatório',
+  min: (
+    value: number,
+    min: number,
+    message: string = 'Campo inválido'
+  ) => {
+    if (value < min) return message
+
+    return true
+  }
+}
 const imageDefault =
   'https://www.fiscalti.com.br/wp-content/uploads/2021/02/default-user-image.png'
 
 export const ProfessionalSchema = object({
-  email: string().email().required('O Campo Email é Obrigatório'),
-  job_id: number().required(),
-  company_id: number().required(),
-  user_type_id: number().required().default(2),
-  cpf: string().transform((currentValue) =>
-    handleRemoveSpecialCharacters(currentValue)
-  ),
-  rg: string().transform((currentValue) =>
-    handleRemoveSpecialCharacters(currentValue)
-  ),
-  name: string().required('O Campo Nome é Obrigatório'),
-  birth_date: date().transform(
-    (currentValue) => new Date(currentValue)
-  ),
-  start_date: date().transform(
-    (currentValue) => new Date(currentValue)
-  ),
-  weekly_hours: number().transform((currentValue) =>
-    Number(currentValue)
-  ),
-  mounth_hours: number().transform((currentValue) =>
-    Number(currentValue)
-  ),
-  commission: string().required(),
-  function_job: string().required(),
-  fixed_payment_value: number().transform((currentValue) =>
-    Number(currentValue)
-  ),
-  telephone_number: string().transform((currentValue) =>
-    handleRemoveSpecialCharacters(currentValue)
-  ),
-  job_type: string().required(),
-  avatar: string().default(imageDefault),
-  cep: string().transform((currentValue) =>
-    handleRemoveSpecialCharacters(currentValue)
-  ),
-  street_name: string().required(),
-  house_number: number().transform((currentValue) =>
-    Number(currentValue)
-  ),
-  neighbourhood_name: string().required(),
-  city_name: string().required(),
-  country: string().required().default('Brasil'),
-  uf: string().required(),
-  projects: array(
-    object({
-      id: number(),
-      name: string(),
-      date_start: string(), //date
-      extra_hours_estimated: number(),
-      extra_hours_performed: number(),
-      extra_hours_percent: number(),
-      hours_mounths_estimated: number(),
-      hours_mounths_performed: number(),
-      hours_mounths_percent: number()
-    })
-  ).default([]),
+  email: string().email().required(validation.required),
+  job_id: mixed().test({
+    name: 'required',
+    message: validation.required,
+    test: (value) => value && value.value !== '' && value.label !== ''
+  }),
+  company_id: number().required(validation.required),
+  user_type_id: number().required(validation.required).default(2),
+  cpf: string()
+    .transform((currentValue) =>
+      handleRemoveSpecialCharacters(currentValue)
+    )
+    .required(validation.required),
+  rg: string()
+    .transform((currentValue) =>
+      handleRemoveSpecialCharacters(currentValue)
+    )
+    .required(validation.required),
+  name: string().required(validation.required),
+  birth_date: string().required(validation.required),
+  start_date: string().required(validation.required),
+  weekly_hours: number()
+    .typeError(validation.required)
+    .required(validation.required),
+  mounth_hours: number()
+    .typeError(validation.required)
+    .required(validation.required),
+  commission: string().required(validation.required),
+  fixed_payment_value: number()
+    .typeError(validation.required)
+    .required(validation.required),
+  telephone_number: string()
+    .transform((currentValue) =>
+      handleRemoveSpecialCharacters(currentValue)
+    )
+    .required(validation.required),
+  job_type: mixed().test({
+    name: 'required',
+    message: validation.required,
+    test: (value) => value && value.value !== '' && value.label !== ''
+  }),
+  cep: string()
+    .transform((currentValue) =>
+      handleRemoveSpecialCharacters(currentValue)
+    )
+    .required(validation.required),
+  street_name: string().required(validation.required),
+  house_number: number()
+    .typeError(validation.required)
+    .required(validation.required),
+  neighbourhood_name: string().required(validation.required),
+  city_name: string().required(validation.required),
+  country: string().required(validation.required).default('Brasil'),
+  uf: mixed().test({
+    name: 'required',
+    message: validation.required,
+    test: (value) => value && value.value !== '' && value.label !== ''
+  }),
   professional_data: object({
-    bank: string().required(),
-    account_type: string().required(),
-    agency: number().required(),
-    account_number: number().required(),
-    type_person: string().required(),
-    type_of_transfer: string().required()
+    bank: mixed().test({
+      name: 'required',
+      message: validation.required,
+      test: (value) =>
+        value && value.value !== '' && value.label !== ''
+    }),
+    account_type: mixed().test({
+      name: 'required',
+      message: validation.required,
+      test: (value) =>
+        value && value.value !== '' && value.label !== ''
+    }),
+    agency: number()
+      .typeError(validation.required)
+      .required(validation.required),
+    account_number: string().required(validation.required),
+    type_person: mixed().test({
+      name: 'required',
+      message: validation.required,
+      test: (value) =>
+        value && value.value !== '' && value.label !== ''
+    }),
+    type_of_transfer: mixed().test({
+      name: 'required',
+      message: validation.required,
+      test: (value) =>
+        value && value.value !== '' && value.label !== ''
+    })
   })
 })
