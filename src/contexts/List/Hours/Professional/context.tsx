0f07540135c015e517cdcc2ef11c71845cfea932
EmanoelMittmann <emanoelmittmann@hotmail.com>
2023-07-27 14:49:58 -0300

    fix: [Hours Professional] correction in fetch of options of filters

--- src/contexts/List/Hours/Professional/context.tsx
+++ src/contexts/List/Hours/Professional/context.tsx
@@ -77,11 +77,11 @@ export const Provider = ({ children }: { children: ReactNode }) => {
           await api.get(routes.projectUsers.list)
         ])
       setFilterOptions({
-        project: project.data.map((prop: any) => ({
+        project: project.map((prop: any) => ({
           label: prop.name,
           value: prop.id
         })),
-        status: extraHour.data.map((prop: any) => ({
+        status: extraHour.map((prop: any) => ({
           label: prop.name,
           value: prop.id
         }))
@@ -144,7 +144,7 @@ export const Provider = ({ children }: { children: ReactNode }) => {
   })
 
   useDebounce({
-    fn: fetchFilters,
+    fn: () => fetchFilters(),
     listener: []
   })
 
