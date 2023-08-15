b85ba87bdf7c7e911c6d130637594af87b30a6c1
EmanoelMittmann <emanoelmittmann@hotmail.com>
2023-08-15 11:19:52 -0300

    refactor: not finish

--- src/components/organisms/Forms/Team/logic.ts
+++ src/components/organisms/Forms/Team/logic.ts
@@ -1,9 +1,11 @@
+import { UseFormReturn } from 'react-hook-form'
+
 import { toast } from '@stardust-ds/react'
 
 import api from 'api'
 import { routes } from 'routes'
 
-import { TeamMemberProps } from './types'
+import { FormTeamProps, TeamMemberProps } from './types'
 
 export async function bindUserAtProject(
   id: number,
@@ -46,4 +48,94 @@ export function convertDateFormat(inputDate: string) {
   const year = parts[3]
 
   return `${day}/${month}/${year}`
+}
+
+export function handleTeam(
+  Context: UseFormReturn<FormTeamProps>,
+  ProjectId: number
+) {
+  const {
+    watch,
+    setValue,
+    getValues,
+    setError,
+    formState: { errors }
+  } = Context
+
+  const { jobs, users, professional } = watch()
+  const { avatar } = professional
+  const job_ = watch('jobs.name.label')
+  const status = users.status
+  const hoursMonth = users.hours_mounths_estimated | 0
+  const techLead = users.isTechLead
+  const job = jobs
+
+  if (professional && jobs) {
+    const validationToIncludeTeam = (errorMessage: string) => ({
+      type: 'manual',
+      message: errorMessage
+    })
+
+    if (!professional.name) {
+      setError(
+        'professional.name',
+        validationToIncludeTeam(
+          'Campo vazio, selecione um profissional!'
+        )
+      )
+      return
+    }
+    if (!jobs.name) {
+      setError(
+        'jobs.name',
+        validationToIncludeTeam('Campo vazio selecione um cargo!')
+      )
+      return
+    }
+    if (!hoursMonth || hoursMonth <= 0) {
+      setError(
+        'users.hours_mounths_estimated',
+        validationToIncludeTeam(
+          'O Campo Hora/ mês deve ser maior que 0!'
+        )
+      )
+      return
+    }
+
+    const newTeamMember = {
+      user_id: professional.name.value,
+      professional,
+      date_start_allocation: users.date_start_allocation,
+      job,
+      job_: techLead ? 'Tech Lead' : job_,
+      isTechLead: techLead,
+      extra_hours_estimated: users.extra_hours_estimated | 0,
+      hours_mounths_estimated: hoursMonth,
+      hours_mounths_performed: users.hours_mounths_performed | 0,
+      extra_hours_performed: users.extra_hours_performed | 0,
+      status: status ? 'Ativo' : 'Inativo',
+      avatar: avatar
+        ? avatar
+        : 'https://www.fiscalti.com.br/wp-content/uploads/2021/02/default-user-image.png'
+    } as unknown as TeamMemberProps
+
+    const currentTeam = getValues('team') || []
+    const newTeam = [...currentTeam, newTeamMember]
+
+    if (ProjectId) {
+      bindUserAtProject(Number(ProjectId), newTeamMember)
+    }
+
+    setValue('team', newTeam)
+    toast({
+      title: 'Profissional cadastrado com sucesso!',
+      type: 'success',
+      position: 'bottom-right'
+    })
+  }
+  setValue('professional.name', null)
+  setValue('jobs.name', null)
+  setValue('users.date_start_allocation', undefined)
+  setValue('users.hours_mounths_estimated', 0)
+  return
 }
