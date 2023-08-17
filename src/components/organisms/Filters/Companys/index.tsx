7732bdee885c97c6c2637f729fd1f4ce44dd6967
EmanoelMittmann <emanoelmittmann@hotmail.com>
2023-08-17 16:03:53 -0300

    fix: remove width, height and alter name const nagivate for navigate

--- src/components/organisms/Filters/Companys/index.tsx
+++ src/components/organisms/Filters/Companys/index.tsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
-
 import { Input, Select } from '@stardust-ds/react'
 import { List } from 'contexts'
 
@@ -10,7 +9,7 @@ import { Container, Main } from '../style'
 import { Option } from 'types'
 
 export const Companys = () => {
-  const nagivate = useNavigate()
+  const navigate = useNavigate()
   const {
     meta,
     filterOptions,
@@ -25,8 +24,6 @@ export const Companys = () => {
       <Container gap='1em'>
         <Input
           value={meta.search}
-          width={230}
-          height={42}
           placeholder='Buscar...'
           style={{
             textOverflow: 'ellipsis',
@@ -42,7 +39,6 @@ export const Companys = () => {
           searchable
           options={filterOptions.registration}
           placeholder='Situações Cadastrais'
-          width={230}
           onClear={() => handleRegistration(null)}
           onSelect={(option: Option | null) =>
             option && handleRegistration(option.value)
@@ -52,7 +48,6 @@ export const Companys = () => {
           searchable
           options={filterOptions.uf}
           placeholder='UF'
-          width={230}
           onSelect={(option: Option | null) =>
             option && handleUf(option.value)
           }
@@ -62,14 +57,13 @@ export const Companys = () => {
           searchable
           options={filterOptions.typeCompany}
           placeholder='Tipos de Empresas'
-          width={230}
           onSelect={(option: Option | null) =>
             option && handleTypeCompany(option.value)
           }
           onClear={() => handleTypeCompany(null)}
         />
       </Container>
-      <Button.New onClick={() => nagivate('/company/new')} />
+      <Button.New onClick={() => navigate('/company/new')} />
     </Main>
   )
 }
