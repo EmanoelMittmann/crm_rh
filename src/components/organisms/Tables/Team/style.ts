6ac286b4919126e546439548b25a0c1cf994e790
Cicera Ribeiro <maccicera@gmail.com>
2023-08-24 09:21:51 -0300

    fix: Ajustando lista de profissional para receber apenas o profissionais ativos em time de projeto

---
+++ src/components/organisms/Tables/Team/style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const ScrollContainer = styled.ul`
+  overflow-y: scroll;
+  height: 385px;
+  width: 100%;
+  margin-bottom: 0.5em;
+  border-bottom: 1px solid #ccc;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: #eee;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 20px;
+    border: 3px solid #ddd;
+    background-color: #ddd;
+  }
+`
