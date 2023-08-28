9f767c7eea5efbccd172b0864e24f033597f2085
EmanoelMittmann <emanoelmittmann@hotmail.com>
2023-08-28 12:35:09 -0300

    fix: added column date_end_allocation and corretion align in table

--- src/components/organisms/Tables/Team/Shelf.tsx
+++ src/components/organisms/Tables/Team/Shelf.tsx
@@ -25,6 +25,7 @@ export const Shelf = ({
     name,
     professional,
     date_start_allocation,
+    date_end_allocation,
     status,
     job_,
     avatar
@@ -41,11 +42,14 @@ export const Shelf = ({
           <TextJob>{job_}</TextJob>
         </TeamJobName>
       </ContainerShelfColumn>
-      <ContainerShelfColumn left='2.8em'>
+      <ContainerShelfColumn left='.5em'>
         <Text>{hours_mounths_estimated}</Text>
       </ContainerShelfColumn>
-      <ContainerShelfColumn left='2.5em'>
+      <ContainerShelfColumn left='.5em'>
         <Text>{formatDate(date_start_allocation)}</Text>
+      </ContainerShelfColumn>
+      <ContainerShelfColumn left='.5em'>
+        <Text>{formatDate(date_end_allocation)}</Text>
       </ContainerShelfColumn>
       <ContainerShelfColumn justify='start'>
         <Badge.Status status={status} />
